9a2f6eb0a849b50d5745173253c45974097ed40f
Nyole <you@example.com>
2024-05-23 00:02:01 +0300

    complete

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i>0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+    # pass
 
 def square_integers(int_list):
-    # code goes here!
+    return [k**2 for k in int_list]
     pass
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for x in range(1, 101):
+        if x %3 ==0 and x %5 == 0:
+            print("FizzBuzz")
+        elif x %3 ==0:
+            print("Fizz")
+        elif x %5 ==0:
+            print("Buzz")
+        else:
+            print(x)
+    # pass
